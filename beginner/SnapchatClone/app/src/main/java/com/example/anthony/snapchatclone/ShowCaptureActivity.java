186211fc8135cfd60e841009c8d7345ddd8bb38c
vanstorm9 <antlowhur@yahoo.com>
2018-03-24 18:46:13 -0400

    Can send images to Firebase database

--- beginner/SnapchatClone/app/src/main/java/com/example/anthony/snapchatclone/ShowCaptureActivity.java
+++ beginner/SnapchatClone/app/src/main/java/com/example/anthony/snapchatclone/ShowCaptureActivity.java
@@ -3,15 +3,27 @@ package com.example.anthony.snapchatclone;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShowCaptureActivity extends AppCompatActivity {
 
@@ -35,20 +47,59 @@ public class ShowCaptureActivity extends AppCompatActivity {
         ImageView mImage = findViewById(R.id.imageCaptured);
         mImage.setImageBitmap(bitmap);
 
+
+        // Dealing with stories
         Uid = FirebaseAuth.getInstance().getUid();
 
-        /*
-        Button mSend = findViewById(R.id.send);
-        mSend.setOnClickListener(new View.OnClickListener() {
+        Button mStory = findViewById(R.id.story);
+        mStory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ChooseReceiverActivity.class);
-                startActivity(intent);
+                saveToStories();
+            }
+
+
+        });
+
+    }
+
+    private void saveToStories() {
+        final DatabaseReference userStoryDb = FirebaseDatabase.getInstance().getReference().child("users").child(Uid).child("story");
+        final String key = userStoryDb.push().getKey();
+
+        StorageReference filePath = FirebaseStorage.getInstance().getReference().child("captures").child(key);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+        byte [] dataToUpload = baos.toByteArray();
+        UploadTask uploadTask = filePath.putBytes(dataToUpload);
+
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri imageUrl = taskSnapshot.getDownloadUrl();
+
+                Long currentTimeStamp = System.currentTimeMillis();
+                Long endTimeStamp = System.currentTimeMillis() + (24*60*60*1000);
+
+                Map<String,Object> mapToUpload = new HashMap<>();
+                mapToUpload.put("imageURL",imageUrl.toString());
+                mapToUpload.put("timestampBeg",currentTimeStamp.toString());
+                mapToUpload.put("timestampEnd",endTimeStamp.toString());
+
+                userStoryDb.child(key).setValue(mapToUpload);
+                finish();
                 return;
             }
         });
-        */
 
-
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                finish();
+                return;
+            }
+        });
     }
 }
