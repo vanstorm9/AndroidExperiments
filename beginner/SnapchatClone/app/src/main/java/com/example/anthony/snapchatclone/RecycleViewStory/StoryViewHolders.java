36faccbfbb6a1c693d505cfc39bc9710f8e53b8f
vanstorm9 <antlowhur@yahoo.com>
2018-04-07 05:46:27 -0400

    Attempting to add view image story mode

--- beginner/SnapchatClone/app/src/main/java/com/example/anthony/snapchatclone/RecycleViewStory/StoryViewHolders.java
+++ beginner/SnapchatClone/app/src/main/java/com/example/anthony/snapchatclone/RecycleViewStory/StoryViewHolders.java
@@ -1,21 +1,35 @@
 package com.example.anthony.snapchatclone.RecycleViewStory;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.anthony.snapchatclone.DisplayImageActivity;
 import com.example.anthony.snapchatclone.R;
 
 /**
  * Created by anthony on 3/24/18.
  */
 
-public class StoryViewHolders extends RecyclerView.ViewHolder{
+public class StoryViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
     public TextView mEmail;
 
     public StoryViewHolders(View itemView){
         super(itemView);
+        itemView.setOnClickListener(this);
         mEmail = itemView.findViewById(R.id.email);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), DisplayImageActivity.class);
+        Bundle b = new Bundle();
+        b.putString("userId", mEmail.getTag().toString());
+        intent.putExtras(b);
+        view.getContext().startActivity(intent);
+
     }
 }
